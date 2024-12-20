780faf81c34b5a2e17e1ceda90778c84099a997d
Katja Gabrysch <kattr302@user.uu.se>
2024-12-20 12:56:24 +0100

    Added new tab_organ table, old tab_organ renamed to tab_organ_2023

--- R/data.R
+++ R/data.R
@@ -32,9 +32,10 @@
 #' \item{E_inh}{µSv}
 #' }
 #' @encoding UTF-8
-#' @source Tondel M, Gabrysch K, Rääf C, Isaksson M. (2024)
+#' @source Tondel M, Gabrysch K, Rääf C, Isaksson M. (2025)
 #' A model for estimating radiation doses and population cancer risk in Sweden
-#' after the Chernobyl Nuclear Power Plant accident in 1986. Uppsala University. doi:.
+#' after the Chernobyl Nuclear Power Plant accident in 1986. Uppsala University.
+#' doi:10.33063/diva-544175.
 "tab_county"
 
 #' County dependent parameters used in 2023
@@ -87,9 +88,10 @@
 #' \item{hunter_perc}{percentage of hunter households}
 #' }
 #' @encoding UTF-8
-#' @source Tondel M, Gabrysch K, Rääf C, Isaksson M. (2024)
+#' @source Tondel M, Gabrysch K, Rääf C, Isaksson M. (2025)
 #' A model for estimating radiation doses and population cancer risk in Sweden
-#' after the Chernobyl Nuclear Power Plant accident in 1986. Uppsala University. doi:.
+#' after the Chernobyl Nuclear Power Plant accident in 1986. Uppsala University.
+#' doi:10.33063/diva-544175.
 "tab_municip"
 
 #' Cancer site dependent parameters
@@ -99,6 +101,9 @@
 #'  and
 #' \eqn{\epsilon}_Cs_137,
 #' per cancer site (organ) and per sex.
+#'
+#'`r lifecycle::badge("superseded")`
+#' Replaced with \code{\link{tab_organ}}.
 #'
 #'@encoding UTF-8
 #' @format A data set in data.table format with 23 rows and 15 variables:
@@ -116,6 +121,38 @@
 #' }
 #' @encoding UTF-8
 #' @source `r citation("absorbedDose")$textVersion`
+"tab_organ_2023"
+
+#' Cancer site dependent parameters
+#'
+#' A table with values of k_SEQ, coefficients of organ dependent polynomials,
+#' \eqn{\epsilon}_Cs-134
+#'  and
+#' \eqn{\epsilon}_Cs_137,
+#' per cancer site (organ) and per sex.
+#'
+#'@encoding UTF-8
+#' @format A data set in data.table format with 33 rows and 17 variables:
+#' \describe{
+#' \item{Organ}{names of the organs}
+#' \item{EPA_cancer_site}{organ/tissues included by EPA (2011) that are used
+#' for computation of lifetime attributable risk (LAR)}
+#' \item{ICRP_risk_organ}{organ/tissues included by ICRP that are used for computation of effective dose}
+#' \item{k_SEQ_organ_f, k_SEQ_organ_m}{k_SEQ values for females and males, respectively}
+#' \item{a0_f, a1_f, a2_f, a3_f}{coefficients of the polynomial for females}
+#' \item{a0_m, a1_m, a2_m, a3_m}{coefficients of the polynomial for males}
+#' \item{Cs134_f, Cs134_m}{\eqn{\epsilon}
+#'  values for Cs-134 for
+#' females and males, respectively}
+#' \item{Cs137_f, Cs137_m}{\eqn{\epsilon}
+#' values for Cs-137 for
+#' females and males, respectively}
+#' }
+#' @encoding UTF-8
+#' @source Tondel M, Gabrysch K, Rääf C, Isaksson M. (2025)
+#' A model for estimating radiation doses and population cancer risk in Sweden
+#' after the Chernobyl Nuclear Power Plant accident in 1986. Uppsala University.
+#' doi:10.33063/diva-544175.
 "tab_organ"
 
 #' Tissue weighting factors
@@ -133,3 +170,37 @@
 #' international commission on radiological protection. Ann. ICRP 37.2–4, pp. 1–332.
 #' doi: 10.1016/j.icrp.2007.10.003.
 "tab_wT"
+
+#' LAR coefficients for males
+#'
+#' Lifetime attributable risk (LAR) for cancer incidence (cases per 10 000 person-Gy)
+#' by age at exposure for males. The coefficients are taken from EPA (2011).
+#' In addition, we set coefficients to 0 at age 100.
+#'
+#'@encoding UTF-8
+#' @format A data set in data.table format with 12 rows and 15 variables:
+#' \describe{
+#' \item{Age}{age at exposure}
+#' \item{cancer sites}{LAR for cancer incidnce of the organ by age}
+#' }
+#' @encoding UTF-8
+#' @source EPA (2011). Radiogenic Cancer Risk Models and Projections for the U.S. Population.
+#' EPA 402-R-11-001. Washington, DC: U.S. Environmental Protection Agency.
+"tab_LAR_M"
+
+#' LAR coefficients for females
+#'
+#' Lifetime attributable risk (LAR) for cancer incidence (cases per 10 000 person-Gy)
+#' by age at exposure for females. The coefficients are taken from EPA (2011).
+#' In addition, we set coefficients to 0 at age 100.
+#'
+#'@encoding UTF-8
+#' @format A data set in data.table format with 12 rows and 17 variables:
+#' \describe{
+#' \item{Age}{age at exposure}
+#' \item{cancer sites}{LAR for cancer incidnce of the organ by age}
+#' }
+#' @encoding UTF-8
+#' @source EPA (2011). Radiogenic Cancer Risk Models and Projections for the U.S. Population.
+#' EPA 402-R-11-001. Washington, DC: U.S. Environmental Protection Agency.
+"tab_LAR_W"
